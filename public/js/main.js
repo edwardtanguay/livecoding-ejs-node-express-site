1426a9c27199a9fa38824e6f4ae64c9adfdf1367
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-01 11:31:36 +0200

    image clickable

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,8 @@
 const imgConferenceElem = document.querySelector('.imgConference');
 const infoElem = document.querySelector('.info');
+
+imgConferenceElem.addEventListener('click', () => {
+	infoElem.innerHTML = `
+This conference will be informative to those wanting to learn Python and machine learning algorithms.	
+	` 
+});
