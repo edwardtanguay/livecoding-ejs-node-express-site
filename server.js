41cd5dafedd7339754fb221f26abcf9c554cdb14
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-01 10:13:26 +0200

    have web site

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve(path.dirname(''));
+
+const app = express();
+const port = 3033;
+
+const staticDirectory = path.join(__dirname, './public');
+app.use(express.static(staticDirectory));
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
